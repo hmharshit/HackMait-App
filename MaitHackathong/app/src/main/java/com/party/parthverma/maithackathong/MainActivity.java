320dd3602bd2e5548b8429d15d029deb0817ae7b
Parth Verma <v.parth98@gmail.com>
2017-04-07 12:10:54 +0530

    WIP: Permissions requesting

--- MaitHackathong/app/src/main/java/com/party/parthverma/maithackathong/MainActivity.java
+++ MaitHackathong/app/src/main/java/com/party/parthverma/maithackathong/MainActivity.java
@@ -4,18 +4,37 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    String[] perms ={"android.permission.CALL_PHONE"};
+    int permsRequestCode = 200;
+    @Override
+    public void onRequestPermissionsResult(int permsRequestCode, String[] permissions, int[] grantResults) {
+        switch (permsRequestCode) {
 
+            case 200:
+
+                boolean callaccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+
+
+                break;
+
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("ABDHABAKS:",Integer.toString(Build.VERSION.SDK_INT));
+        if(Build.VERSION.SDK_INT>Build.VERSION_CODES.LOLLIPOP_MR1)
+            requestPermissions(perms,permsRequestCode);
 
     }
 
