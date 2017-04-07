d7da2760f6bbf93e966646f9093d50554c4383d1
Tushar Singh Bhal <tushar.rocks97@gmail.com>
2017-04-07 13:58:27 +0530

    added semi functioning action bar

--- MaitHackathong/app/src/main/java/com/party/parthverma/maithackathong/MainActivity.java
+++ MaitHackathong/app/src/main/java/com/party/parthverma/maithackathong/MainActivity.java
@@ -7,8 +7,10 @@ import android.net.Uri;
 import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
@@ -32,6 +34,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar=(Toolbar)findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setTitle("First Aid");
+        ActionBar ab= getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+
         Log.d("ABDHABAKS:",Integer.toString(Build.VERSION.SDK_INT));
         if(Build.VERSION.SDK_INT>Build.VERSION_CODES.LOLLIPOP_MR1)
             requestPermissions(perms,permsRequestCode);
