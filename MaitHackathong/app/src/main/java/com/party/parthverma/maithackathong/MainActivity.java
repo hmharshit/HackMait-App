333bbabfa74a4a14e0a9acf9f640722edb1a41ac
Parth Verma <v.parth98@gmail.com>
2017-04-07 11:35:28 +0530

    Added methods to place calls

--- MaitHackathong/app/src/main/java/com/party/parthverma/maithackathong/MainActivity.java
+++ MaitHackathong/app/src/main/java/com/party/parthverma/maithackathong/MainActivity.java
@@ -1,7 +1,14 @@
 package com.party.parthverma.maithackathong;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void callAmbulance(View view)
+    {
+        place_call("102");
+
+    }
+    public void callPolice(View view)
+    {
+        place_call("100");
+    }
+    public void callFirebrigade(View view)
+    {
+        place_call("101");
+    }
+    public void place_call(String num)
+    {
+        String uri = "tel:"+num;
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse(uri));
+        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE);
+        if(permission == PackageManager.PERMISSION_GRANTED)
+            startActivity(intent);
     }
 }
